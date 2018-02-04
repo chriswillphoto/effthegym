8ec5f09e7909c1c84a9844024f83f7903642945b
chriswillphoto <christophernekich@gmail.com>
2018-02-04 12:19:14 +1100

    added workout list viewer

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './Workouts.css'
+import './Workouts.css';
 
-const Workouts = () => {
-  return (
-    <div className="workouts">
-      <h1>Workout List</h1>
-    </div>
-  );
+const Workouts = props => {
+  if (props.workouts) {
+    return (
+      <div className="workouts-container">
+        {props.workouts.map(workout => {
+          return (
+            <a href="#" key={workout.id}>
+              {workout.name}
+            </a>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className="workouts-container">
+        <h1> Add New Workout </h1>
+      </div>
+    );
+  }
 };
 
 export default Workouts;
