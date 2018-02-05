b17c6c37ece8a9b42519500aca5200b6d0dd7889
chriswillphoto <christophernekich@gmail.com>
2018-02-05 15:24:38 +1100

    updated conditional render of buttonlist in workout component

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -2,25 +2,24 @@ import React from 'react';
 import './Workouts.css';
 
 const Workouts = props => {
-  if (Object.keys(props.workouts).length > 0) {
-    return (
-      <div className="workouts-container">
-        {Object.values(props.workouts).map(workout => {
-          return (
-            <button key={workout.id} onClick={() => props.selectWorkout(workout)}>
-              {workout.name}
-            </button>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return (
-      <div className="workouts-container">
-        <h1> Add New Workout </h1>
-      </div>
-    );
-  }
+  const workoutlist = Object.keys(props.workouts);
+  console.log(workoutlist);
+  const listRender = function(prop) {
+    return Object.values(prop).map(workout => {
+      return (
+        <button key={workout.id} onClick={() => props.selectWorkout(workout)}>
+          {workout.name}
+        </button>
+      );
+    });
+  };
+
+  return (
+    <div className="workouts-container">
+      {workoutlist.length > 0 && listRender(props.workouts)}
+      <h1> Add New Workout </h1>
+    </div>
+  );
 };
 
 export default Workouts;
