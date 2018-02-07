8115c72c5b7ebe91abab454ccd788d46bd3364a7
chriswillphoto <christophernekich@gmail.com>
2018-02-07 23:09:11 +1100

    run now class based component

--- src/components/Run.js
+++ src/components/Run.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {PureComponent as Component} from 'react';
 
-const Run = (props) => {
-  return(
-    <div>
-      <h1>Run Coming Soon</h1>
-    </div>
-  )
+class Run extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      workout: this.props.workout,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Run Coming Soon</h1>
+      </div>
+    );
+  }
 }
 
-export default Run
+export default Run;
