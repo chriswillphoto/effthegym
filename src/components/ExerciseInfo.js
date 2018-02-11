c8f0b0684125c4ed5ee882c1dc1656b209f35bfd
chriswillphoto <christophernekich@gmail.com>
2018-02-11 12:59:38 +1100

    delete exercise from workout moved to x button

--- src/components/ExerciseInfo.js
+++ src/components/ExerciseInfo.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ExerciseInfo = props => {
   if (props.exercise.sets) {
     return (
-      <div onClick={() => props.delete()}>
+      <div>
         <h4>{props.exercise.name}</h4>
         <h6>Sets: {props.exercise.sets}</h6>
         <h6>Reps: {props.exercise.reps}</h6>
+        <button onClick={() => props.delete()}>X</button>
       </div>
     );
   } else {
     return (
-      <div onClick={() => props.delete()}>
+      <div>
         <h4>{props.exercise.name}</h4>
         <h6>Time: {props.exercise.time} seconds</h6>
+        <button onClick={() => props.delete()}>X</button>
       </div>
     );
   }
