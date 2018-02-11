39994f6ce307da72938e968fe234f107bd9e404d
chriswillphoto <christophernekich@gmail.com>
2018-02-11 17:25:32 +1100

    added message for if there are no workouts in list

--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import AddWorkout from './AddWorkout';
-import './Workouts.css';
-import Nav from './Nav';
-import ExerciseInfo from './ExerciseInfo'
-import uniqid from 'uniqid'
-
-
+import React from "react";
+import AddWorkout from "./AddWorkout";
+import "./Workouts.css";
+import Nav from "./Nav";
+import ExerciseInfo from "./ExerciseInfo";
+import uniqid from "uniqid";
 
 const Workouts = props => {
   const workoutlist = Object.keys(props.workouts);
@@ -23,14 +21,17 @@ const Workouts = props => {
               Go
             </a>
             <a href="/#/workout/edit" onClick={() => props.selectWorkout(workout.id)}>
-             Add Exercises to this Workout 
+              Add Exercises to this Workout
             </a>
           </div>
           {workout.exercises.map(exercise => {
             return (
               <div key={uniqid()} className="exercise-container">
-                <ExerciseInfo delete={() => props.delExercise(exercise, workout.id)} exercise={exercise}/>
-              </div>  
+                <ExerciseInfo
+                  delete={() => props.delExercise(exercise, workout.id)}
+                  exercise={exercise}
+                />
+              </div>
             );
           })}
         </div>
@@ -41,6 +42,7 @@ const Workouts = props => {
   return (
     <div className="workouts-container">
       <Nav address="/#/" />
+      {workoutlist.length === 0 && <h2 className="empty-header"> You Haven't Created Any Workouts! Add One Below. </h2>}
       {workoutlist.length > 0 && listRender(props.workouts)}
       <AddWorkout addWorkout={() => props.onAddClick()} />
     </div>
