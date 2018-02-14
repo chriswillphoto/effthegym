6aef4224aac50956ba603269455789d33bc95505
chriswillphoto <christophernekich@gmail.com>
2018-02-14 11:14:34 +1100

    playing with run page styling

--- src/components/Run.js
+++ src/components/Run.js
@@ -62,9 +62,9 @@ class Run extends Component {
             paused={this.state.pauseTimer}
           />
           {this.state.pauseTimer ? (
-            <button onClick={() => this.setState({ pauseTimer: false })}>Go</button>
+            <button className="time-button" onClick={() => this.setState({ pauseTimer: false })}>Go</button>
           ) : (
-            <button onClick={() => this.setState({ pauseTimer: true })}>Pause</button>
+            <button className="time-button" onClick={() => this.setState({ pauseTimer: true })}>Pause</button>
           )}
         </div>
       );
