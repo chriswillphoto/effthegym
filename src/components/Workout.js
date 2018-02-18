71b178f539599f3d99150430009c96cd0439ca1d
chriswillphoto <christophernekich@gmail.com>
2018-02-18 14:19:46 +1100

    prettier

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,6 +1,6 @@
 import React, { PureComponent as Component } from "react";
 import ExerciseInfo from "./ExerciseInfo";
-import uniqid from 'uniqid'
+import uniqid from "uniqid";
 
 export default class Workout extends Component {
   constructor(props) {
@@ -29,14 +29,18 @@ export default class Workout extends Component {
             Go
           </a>
           <ol className={"exercise-container" + (workout.exercises.length === 0 ? " empty" : "")}>
-          {workout.exercises.map(exercise => {
-            return(
-              <ExerciseInfo key={uniqid()} delete={() => this.props.delExercise(exercise, workout.id)} exercise={exercise} />
-            )
-          })}
+            {workout.exercises.map(exercise => {
+              return (
+                <ExerciseInfo
+                  key={uniqid()}
+                  delete={() => this.props.delExercise(exercise, workout.id)}
+                  exercise={exercise}
+                />
+              );
+            })}
           </ol>
           <a href="/#/workout/edit" onClick={() => this.props.select(workout.id)}>
-          Add Exercises to this Workout
+            Add Exercises to this Workout
           </a>
           <button onClick={() => this.props.deleteWorkout(workout)}>Delete this Workout</button>
         </div>
