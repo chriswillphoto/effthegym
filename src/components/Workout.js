3d84f9d16697692673eea45190da8f0f410dc507
chriswillphoto <christophernekich@gmail.com>
2018-02-18 14:18:20 +1100

    single workouts in list are now their own component

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { PureComponent as Component } from "react";
+import ExerciseInfo from "./ExerciseInfo";
+import uniqid from 'uniqid'
 
-const Workout = (props) => {
-  return(
-    <div key={props.workout.id} onClick={() => props.selectWorkout(workout.id)}>
+export default class Workout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showInfo: false
+    };
+  }
+
+  render() {
+    const workout = this.props.workout;
+    return (
+      <div
+        className="workout-container"
+        key={workout.id}
+        onClick={() => this.props.select(workout.id)}
+      >
+        <div className="workout-buttons">
           <h4>{workout.name}</h4>
-          {workout.exercises.map(exercise => {
-            return (
-              <div key={exercise.id} className="exercise-container">
-                <h4>{exercise.info}</h4>
-                <h6>sets: {exercise.sets}</h6>
-              </div>
-            );
-          })}
           <a
-            href="/#/workout/edit"
-            onClick={() => props.selectWorkout(workout.id)}>
-            Edit
+            className="go-button"
+            href="/#/workout/run"
+            onClick={() => this.props.select(workout.id)}
+          >
+            Go
           </a>
+          <ol className={"exercise-container" + (workout.exercises.length === 0 ? " empty" : "")}>
+          {workout.exercises.map(exercise => {
+            return(
+              <ExerciseInfo key={uniqid()} delete={() => this.props.delExercise(exercise, workout.id)} exercise={exercise} />
+            )
+          })}
+          </ol>
+          <a href="/#/workout/edit" onClick={() => this.props.select(workout.id)}>
+          Add Exercises to this Workout
+          </a>
+          <button onClick={() => this.props.deleteWorkout(workout)}>Delete this Workout</button>
         </div>
-  )
+      </div>
+    );
+  }
 }
--- src/components/Workouts.js
+++ src/components/Workouts.js
@@ -4,46 +4,13 @@ import "./Workouts.css";
 import Nav from "./Nav";
 import ExerciseInfo from "./ExerciseInfo";
 import uniqid from "uniqid";
+import Workout from './Workout'
 
 const Workouts = props => {
   const workoutlist = Object.keys(props.workouts);
   const listRender = function(prop) {
     return Object.values(prop).map(workout => {
-      return (
-        <div
-          className="workout-container"
-          key={workout.id}
-          onClick={() => props.selectWorkout(workout.id)}
-        >
-          <div className="workout-buttons">
-            <h4>{workout.name}</h4>
-            <a
-              className="go-button"
-              href="/#/workout/run"
-              onClick={() => props.selectWorkout(workout.id)}
-            >
-              Go
-            </a>
-            <ol className={"exercise-container" + (workout.exercises.length === 0 ? " empty": "")}>
-            {workout.exercises.map(exercise => {
-              return (
-                  <ExerciseInfo
-                    delete={() => props.delExercise(exercise, workout.id)}
-                    exercise={exercise}
-                  />
-              );
-            })}
-            </ol>
-            <a
-              href="/#/workout/edit"
-              onClick={() => props.selectWorkout(workout.id)}
-            >
-              Add Exercises to this Workout
-            </a>
-            <button onClick={() => props.deleteWorkout(workout)}>Delete this workout</button>
-          </div>
-        </div>
-      );
+      return <Workout key={uniqid()} deleteWorkout={props.deleteWorkout} workout={workout} select={props.selectWorkout} delExercise={props.delExercise}/>
     });
   };
 
